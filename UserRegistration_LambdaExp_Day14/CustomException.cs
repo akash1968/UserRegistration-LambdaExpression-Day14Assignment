6b286a625461929a0abeff5f79341bd2fff76647
Akash <akash.singh670@gmail.com>
2020-10-21 18:38:53 +0530

    [Akash] .Added UC13 to validate user data by using lambda expression in User Registration Problem

---
+++ UserRegistration_LambdaExp_Day14/CustomException.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomException.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akash Kumar Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.ExceptionServices;
+
+namespace UserRegistration_LambdaExp_Day14
+{
+   public class CustomException : ApplicationException
+    {
+        public enum ExceptionType
+        {
+            INVALID_DETAILS
+        }
+        public readonly ExceptionType type;
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
